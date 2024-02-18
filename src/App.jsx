ee1ac18421046e678f2894743a75b26cd0f4dbdf
TasnimAlam001 <tasnimalam.001@gmail.com>
2024-02-19 00:14:32 +0600

    pakhi pakhi

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import BIRDS from 'vanta/src/vanta.birds'
+import './App.css'
+
+function App() {
+  
+  useEffect(()=>{
+    BIRDS({
+      el:"#vanta",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00
+    })
+  },[])
+
+  return (
+    <div className="app">
+      <div className="vanta" id="vanta"> </div>
+    </div>
+  )
+}
+
+export default App
